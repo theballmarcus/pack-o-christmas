baa9fbfd415acb009bdf34a42b70418715a3a810
Testerdasd <testerdasd@gmail.com>
2020-08-27 19:02:18 +0200

    First framework build completed

--- christmasGiftGame/server/server.js
+++ christmasGiftGame/server/server.js
@@ -1,15 +1,21 @@
 var createError = require('http-errors');
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
+var registerRouter = require('./routes/register')
 var notFoundRoute = require('./routes/notFound')
+
 
 var app = express();
 
+var port = 29292
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
+app.use('/register', registerRouter);
 app.use('*', notFoundRoute)
 
 // catch 404 and forward to error handler
@@ -33,3 +40,4 @@ app.use(function(req, res, next) {
 });
 
 module.exports = app;
+module.exports.port = port;
